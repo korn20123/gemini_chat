2964103111091ef35e3eeebb819b0c6830f8af67
korn20123 <aaromzschauer@icloud.com>
2025-08-13 16:48:56 +0200

    added a message history

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
+history = []
 def main():
   while True:
     prompt = input('you: ')
@@ -15,12 +17,15 @@ def main():
     except Exception as e:
       print(e)
 def send(prompt):
+  history.append(types.Content(role='user', parts=[types.Part(text=prompt)]))
   try:
     response = client.models.generate_content(
       model = 'gemini-2.0-flash',
-      contents = prompt
+      contents = history
     )
-    return response.text
+    reply = response.candidates[0].content.parts[0].text
+    history.append(types.Content(role="model", parts=[types.Part(text=reply)]))
+    return reply
   except Exception as e:
     print(e)
 if __name__ == '__main__':
